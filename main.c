3ca8e57e95026f04afd5eb375341136c981c579c
eduardoebs <engeduardobs@gmail.com>
2020-04-25 13:27:38 +0000

    Meu primeiro codigo

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+ * main.c
+ * author 2020 Eduardobs 
+ */
+#include <stdio.h>
+
+/*
+* Função principal
+*/
+int main (int argc, char **argv){
+  printf("Ola mundo sou Eduardo espero defender doutorado 2021.\n");
+  return 0;
+}
