8230b62be26184497027de03cec354adab7896c0
Eduardo Brito <engeduardobs@gmail.com>
2020-08-04 00:58:59 +0000

    Ativ 1 - apresenta ordem inversa dos num digitados

--- main.cpp
+++ main.cpp
@@ -1,44 +1,30 @@
 /*
  * main.cpp
  * author 2020 Eduardobs 
+ * faça um programa que leia 3 num e exiba em ordem inversa a sua entrada
  */
 #include <iostream> // necessario para std::cout, std::cin
 
 /*
 * Função principal
 */
-int main (int argc, char **argv){
-  std::string nacionalidade = "nascido";
-  float altura_m = 1.82 ,peso_kg = 94.5;
-  int idade_year = 1975;
-  char sexo = 'M';
+int main (){
+  int num1, num2, num3;
+
+  std::cout << "entre com o 1 num: ";
+  std::cin >> num1;
+
+  std::cout << "entre com o 2 num: ";
+  std::cin >> num2;
+
+  std::cout << "entre com o 3 num: ";
+  std::cin >> num3;
+
+  std::cout << num3 << "\n";
+  std::cout << num2 << "\n";
+  std::cout << num1 << "\n";
+
+  std::cout << "apresenta ordem inversa digitada: " << num3 << ":" << num2 << ":" << num1 << "\n";
   
-  // int valor1;
-  // int valor2;
-  // int resultado;
-  // std::cout << "entre com valor 1: ";
-  // std::cin >> valor1;
-  // std::cout << "entre com valor 2: ";
-  // std::cin >> valor2;
-  // resultado = valor1 + valor2;
-  // std::cout << resultado;
-
-  std::cout << "entre com altura: ";
-  std::cin >> altura_m;
-  std::cout << "entre com peso: ";
-  std::cin >> peso_kg;
-  std::cout << "entre com idade: ";
-  std::cin >> idade_year;
-  std::cout << "entre com sexo: ";
-  std::cin >> sexo;
-  std::cout << "local de nascimento: ";
-  std::cin >> nacionalidade;
-
-  std::cout << "\naltura: " << altura_m << "\n";
-  std::cout << "peso: " << peso_kg << "\n";
-  std::cout << "idade: " << idade_year << "\n";
-  std::cout << "sexo: " << sexo << "\n";
-  std::cout << "local de nascimento: " << nacionalidade;
-  return 0;
 }
 
