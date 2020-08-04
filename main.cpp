2d9ae8e4b9d611d902bcfb30773a0d545f4431b6
Eduardo Brito <engeduardobs@gmail.com>
2020-08-04 01:46:33 +0000

    Ativ 5 - Faça um programa para calcular a media aritmetica entre dois valores do tipo float utilizando linguagem C++

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
  * main.cpp
  * author 2020 Eduardobs 
- * Faça um algoritmo que leia a idade em anos e expresse-a em dias  
+ * Faça um programa para calcular a media aritmetica entre dois valores do tipo float utilizando linguagem C++  
  */
 #include <iostream> // necessario para std::cout, std::cin
 
@@ -9,10 +9,14 @@
 * Função principal
 */
 int main (){
-  float idade;
+  float media_aritmetica, num1, num2;
+  
+  std::cout << "num1: ";
+  std::cin >> num1;
+  std::cout << "num2: ";
+  std::cin >> num2;
 
-  std::cout << "entre com idade: ";
-  std::cin >> idade; 
-  std::cout << idade * 365; 
+  media_aritmetica = (num1+num2)/2;
+  std::cout << media_aritmetica; 
 }
 
